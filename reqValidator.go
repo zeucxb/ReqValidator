3ea444e031e02ded18a023a35022845f6a72d261
Eliseu Benedito Codinhoto <zeucxb@gmail.com>
2016-09-20 17:39:27 -0300

    ValidateAndPopulate

--- reqValidator.go
+++ reqValidator.go
@@ -1,7 +1,7 @@
 package reqValidator
 
 import (
-	"fmt"
+	"errors"
 	"reflect"
 )
 
@@ -9,28 +9,29 @@ type structure interface {
 	Map() map[string]interface{}
 }
 
-// Validate the types and return a bool
-func Validate(structure structure, itemsMap map[string]interface{}) bool {
-	stMap := structure.Map()
+// Validate make the validation, populate the received pointer and callback if has panics
+func Validate(st interface{}, inputMap map[string]interface{}) (err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			switch x := r.(type) {
+			case string:
+				err = errors.New(x)
+			case error:
+				err = x
+			default:
+				err = errors.New("Unknown panic")
+			}
+		}
+	}()
 
-	if len(stMap) != len(itemsMap) {
-		fmt.Println(len(stMap), len(itemsMap))
-		return false
+	t := reflect.ValueOf(st).Elem()
+
+	for elementName, value := range inputMap {
+		val := t.FieldByName(elementName)
+		val.Set(reflect.ValueOf(value))
 	}
 
-	for name, value := range stMap {
-		if _, ok := itemsMap[name]; !ok {
-			fmt.Println(name)
-			return false
-		}
-
-		if reflect.TypeOf(value).Kind() != reflect.TypeOf(itemsMap[name]).Kind() {
-			fmt.Println(name)
-			return false
-		}
-	}
-
-	return true
+	return nil
 }
 
 // ValidateMap Verify if items exists in map
